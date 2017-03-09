0e53a241a0138f84153aaa37a5ed469674e31ad3
Neil Baskerville-Bridges <NABBridges@gmail.com>
2017-03-09 16:01:12 -0700

    Added colour, name functionality

--- public_html/index.js
+++ public_html/index.js
@@ -4,15 +4,81 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 3000;
 
+var cookie = require('cookies');
+var cookieParser = require('cookie-parser');
+
+var userList = [];
+
 http.listen( port, function () {
     console.log('listening on port', port);
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(cookieParser());
+
+//skeleton taken from expressjs.com tutorials
+app.get('/', function(req, res, next){
+   if(req.cookies.user === undefined || req.cookies.user === null) 
+   {
+       userID = "user" + Math.floor((Math.random()*1000) + 1);
+       res.cookie("userID", userID);
+       userList.push(userID);
+   }
+   
+   if(req.cookies.user === undefined || req.cookies.user === null) 
+   {
+       userColour = "red";
+       res.cookie("userColour", userColour);
+   }
+   
+    next();    
+});
 
 // listen to 'chat' messages
 io.on('connection', function(socket){
-    socket.on('chat', function(msg){
-	io.emit('chat', msg);
+    socket.on('chat', function(msg, id, colour){
+        
+        var words = msg.split(" ");
+        var firstWord = words[0];
+        
+        if(firstWord === "/nickcolor")
+        {
+            var newColour = words[1];
+            socket.emit('changeColour', newColour);
+        }
+            
+        else if(firstWord === "/nick")
+        {
+            var newID = words[1];
+            var index = userList.indexOf(newID);
+
+            if(index === -1)
+            {
+                socket.emit('changeID', newID);
+                userList[userList.indexOf(id)] = newID;
+
+            }
+        }
+        
+        console.log(userList);
+        
+	io.emit('chat', msg, getTime(), id, colour);       
     });
 });
+
+
+function getTime() {
+    var toDisplay = "";
+    var fullDate = new Date();
+    var h = fullDate.getHours();
+    var m = fullDate.getMinutes();
+
+    if(m<10)
+    {
+        m = "0" + m;
+    }
+
+    toDisplay += h + ":" + m + "   ";
+    return toDisplay;
+}
+
+app.use(express.static(__dirname + '/public'));
--- public_html/public/client.js
+++ public_html/public/client.js
@@ -2,11 +2,43 @@
 $(function() {
     var socket = io();
     $('form').submit(function(){
-	socket.emit('chat', $('#m').val());
+	socket.emit('chat', $('#m').val(), getCookie("userID"), getCookie("userColour"));
 	$('#m').val('');
 	return false;
     });
-    socket.on('chat', function(msg){
-	$('#messages').append($('<li>').text(msg));
+    
+    socket.on('chat', function(msg, time, id, colour){
+	$('#messages').append($('<li><b>' + time + '</b>' + '<span style="color:' + colour + '">' + 
+                      id + " </span>" + msg + '</li>'));
     });
+    
+    socket.on('changeColour', function(colour){
+        setCookie('userColour', colour);
+    });
+    
+    socket.on('changeID', function(id){
+        setCookie('userID', id);
+    });
+    
 });
+
+//function found at https://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) === ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) === 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
+function setCookie(cname, cvalue) {
+    document.cookie = cname + "=" + cvalue + ";";
+}
